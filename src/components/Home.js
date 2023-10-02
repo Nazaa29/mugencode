491aa00dc70f2dd5a4362c20007d5a7a21b16af9
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-02 20:32:13 -0300

    home styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="bg-dark-custom h-screen"></div>;
+  return <div className="w-full h-screen bg-dark-custom flex flex-col justify-center items-center">
+    <div className="">
+      <h1 className="text-7xl">MUGENCODE ™</h1>
+      <div className="flex items-start">
+        <div className="w-20 h-1 bg-red-custom my-2"></div>
+        <div className="w-20 h-1 bg-red-custom"></div>
+      </div>
+      <p className="text-xl">Web Developers</p>
+    </div>
+  </div>
+
 };
 
 export default Home;
