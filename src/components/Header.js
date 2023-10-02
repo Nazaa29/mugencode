bf634270bf98ed4c956dac0bf33c492a3ef59404
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-02 19:14:57 -0300

    arreglo m=100

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from "../assets/images/logo.png";
 const Header = () => {
   return (
     <header className="bg-dark-custom text-white py-2 px-8 flex items-center justify-between">
-      <div className="logo w=100">
+      <div className="logo w-32">
         <img src={logo} alt="Logo" className="w-24 h-16 ml-12" />
       </div>
       <div className="social-icons flex">
