76e67aead93dd518d901356981adeba751f53dd2
OriginalKazdov <juandovzak@gmail.com>
2023-10-02 19:11:08 -0300

    Cambios CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import logo from "../assets/images/logo.png";
 
 const Header = () => {
   return (
-    <header className="bg-blue-900 text-white py-4 px-8 flex items-center justify-between">
+    <header className="bg-dark-custom text-white py-2 px-8 flex items-center justify-between">
       <div className="logo w=100">
-        <img src={logo} alt="Logo" className="w-100 h-10" />
+        <img src={logo} alt="Logo" className="w-24 h-16 ml-12" />
       </div>
       <div className="social-icons flex">
         <a
           href="https://github.com/Nazaa29/mugencode"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-2xl mr-4 hover:text-gray-400"
+          className="text-3xl mr-4 hover:text-gray-400"
         >
           <FaGithub />
         </a>
@@ -20,7 +20,7 @@ const Header = () => {
           href="URL-de-Twitter"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-2xl mr-4 hover:text-gray-400"
+          className="text-3xl mr-4 hover:text-gray-400"
         >
           <FaTwitter />
         </a>
@@ -28,7 +28,7 @@ const Header = () => {
           href="URL-de-LinkedIn"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-2xl hover:text-gray-400"
+          className="text-3xl hover:text-gray-400"
         >
           <FaLinkedin />
         </a>
