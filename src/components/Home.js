519173ca0fb6e52d5e7ac9e7e8dedd5fdfc7ee03
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-02 20:47:38 -0300

    lineas en home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Home = () => {
   return <div className="w-full h-screen bg-dark-custom flex flex-col justify-center items-center">
-    <div className="">
+    <div>
       <h1 className="text-7xl">MUGENCODE ™</h1>
-      <div className="flex items-start">
-        <div className="w-20 h-1 bg-red-custom my-2"></div>
-        <div className="w-20 h-1 bg-red-custom"></div>
+      <div className="flex flex-col items-start">
+        <div className="w-20 ml-6 h-1 mt-2 rounded bg-red-custom my-2"></div>
+        <div className="w-20 ml-16 h-1 mt-1 rounded bg-red-custom"></div>
       </div>
       <p className="text-xl">Web Developers</p>
     </div>
