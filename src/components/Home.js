e03521f1b484c328dd2b0aff43d4aa2f363532f5
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-03 18:48:15 -0300

    kaz2

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,24 +16,6 @@ const Home = (props) => {
       </div>
     </section>
   );
-
-  return (
-    <div
-      className="w-full bg-dark-custom flex flex-col justify-center items-center"
-      style={{ minHeight: dynamicHeight }}
-    >
-      <div>
-        <h1 className="text-7xl font-tungsten">MUGENCODE ™</h1>
-        <div className="flex flex-col items-start">
-          <div className="w-20 ml-6 h-1 mt-2 rounded bg-red-custom my-2"></div>
-          <div className="w-20 ml-16 h-1 mt-1 rounded bg-red-custom"></div>
-        </div>
-        <p className="ml-6 mt-2 text-xl font-semibold font-tungsten">
-          Web Developers
-        </p>
-      </div>
-    </div>
-  );
-};
+  }
 
 export default Home;
