94d80a655666d3006f37584655220db403eb7f88
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-04 16:52:03 -0300

    estilo de input final

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,18 +4,18 @@ import Button from '../Button';
 
 const Form = () => {
     const inputStyle = 'w-full p-[10px] outline-none border border-gray-custom bg-dark-custom text-white text-[1em]';
-    const labelStyle = 'absolute left-1 p-[10px] pointer-events-none text-white uppercase';
+    const labelStyle = 'absolute left-1 p-[10px] pointer-events-none duration-300 uppercase';
+    const textareaStyle = 'w-full px-3 h-64 py-2 outline-none border border-gray-custom bg-dark-custom text-white text-[1em] resize-none';
 
     return (    
         <div className="max-w-md mx-auto w-full">
             <form>
-                <Input label="Nombre" type="text" inputStyle={inputStyle} labelStyle={labelStyle}/>
-                <Input label="Email" type="email" inputStyle={inputStyle} labelStyle={labelStyle}/>
-                <div className="mb-4 ">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Idea</label>
+                <Input label="Nombre" type="text" inputStyle={inputStyle} labelStyle={labelStyle} />
+                <Input label="Email" type="email" inputStyle={inputStyle} labelStyle={labelStyle} />
+                <div className="mb-4 relative">
+                    <label className="absolute left-1 p-[10px] pointer-events-none duration-300 uppercase">Your Idea</label>
                     <textarea
-                        className="w-full px-3 py-2 border rounded-lg"
-                        placeholder="Escriba su idea aquí..."
+                        className={textareaStyle}
                         rows="4"
                     ></textarea>
                 </div>
--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,24 +1,34 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Input = ({ label, type, inputStyle, labelStyle}) => {
     const [isFocused, setIsFocused] = useState(false);
+    const inputRef=useRef(null);
 
-    const focusHanlder = () => {
+    const focusHandler = () => {
         setIsFocused(true);
     }
 
     const blurHandler = () =>{
-        setIsFocused(false);
+        if(inputRef.current.value.trim() === ''){
+            setIsFocused(false);
+        }else{
+            setIsFocused(true);
+        }
+        
     }
+    console.log(inputRef)
+
+    const labelDynamic=' text-red-custom translate-x-[10px] translate-y-[-9px] text-[0.75em] p-[0_10px] bg-dark-custom border-l-[1px] border-red-custom border-r-[1px] tracking-[0.25em]';
 
     return (
         <div className="mb-4 relative">
-            <label className={`${labelStyle} ${isFocused ? 'text-red-custom' : 'text-white'}`}>{label}</label>
+            <label className={`${labelStyle} ${isFocused ? `${labelDynamic}` : 'text-white'}`}>{label}</label>
             <input
-                className={inputStyle}
+                className={`${inputStyle} ${isFocused ? 'border-red-custom' : 'border-gray-custom'}`}
                 type={type}
-                onFocus={focusHanlder}
+                onFocus={focusHandler}
                 onBlur={blurHandler}
+                ref={inputRef}
             />
             
         </div>
