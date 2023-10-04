6cf496880d5804c1be3b56e6fe273c47656a200f
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-04 18:12:07 -0300

    padding form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,7 +3,7 @@ import Button from '../Button';
 import { useState, useRef } from "react";
 
 const Form = () => {
-    const inputStyle = 'w-full p-[10px] outline-none border border-gray-custom bg-dark-custom text-white text-[1em]';
+    const inputStyle = 'w-full p-[10px] outline-none border border-gray-custom bg-dark-custom text-white text-[1em] ';
     const labelStyle = 'absolute left-1 py-[10px] px-[10px] pointer-events-none duration-300 uppercase';
     const textareaStyle = 'w-full px-3 h-64 py-2 outline-none border border-gray-custom bg-dark-custom text-white text-[1em] resize-none';
 
@@ -31,7 +31,7 @@ const Form = () => {
             <form>
                 <Input label="Nombre" type="text" inputStyle={inputStyle} labelStyle={labelStyle} />
                 <Input label="Email" type="email" inputStyle={inputStyle} labelStyle={labelStyle} />
-                <div className="mb-4 relative">
+                <div className="mb-4 relative pt-4">
                     <label className={`${labelStyle} ${isFocused ? labelDynamic : 'text-gray-custom'}`}>Your Idea</label>
                     <textarea
                         className={`${textareaStyle} ${isFocused ? 'border-red-custom' : 'border-gray-custom'}`}
--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -21,7 +21,7 @@ const Input = ({ label, type, inputStyle, labelStyle}) => {
     const labelDynamic=' text-red-custom translate-x-[10px] translate-y-[-9px] text-[0.75em] py-[0] px-[10px] bg-dark-custom border-l-[1px] border-red-custom border-r-[1px] tracking-[0.25em]';
 
     return (
-        <div className="mb-4 relative">
+        <div className="mb-4 relative pt-4">
             <label className={`${labelStyle} ${isFocused ? `${labelDynamic}` : 'text-white'}`}>{label}</label>
             <input
                 className={`${inputStyle} ${isFocused ? 'border-red-custom' : 'border-gray-custom'}`}
