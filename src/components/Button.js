b15fdf830d3cab9f46dcc4333bbe06eb308de953
nazitaOG <lopeznazarenoo@gmail.com>
2023-10-05 20:20:27 -0300

    cualquier titulo

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,16 @@ import React from "react";
 import "../assets/styles/boton.css"; // Importa el archivo CSS
 
 const Button = () => {
-  return (
-    <div className="w-[50%] relative buttonContainer">
-      {" "}
-      {/* Agregada clase buttonContainer */}
-      <button class="button type">Send</button>
-      <svg class="svg rect">
-        <rect x="0" y="0" fill="none" width="100%" height="100%" />
-      </svg>
-    </div>
-  );
+    return (
+        <div className="w-[50%] relative buttonContainer">
+            {" "}
+            {/* Agregada clase buttonContainer */}
+            <button class="button type">Send</button>
+            <svg class="svg rect">
+                <rect x="0" y="0" fill="none" width="100%" height="100%" />
+            </svg>
+        </div>
+    );
 };
 
 export default Button;
