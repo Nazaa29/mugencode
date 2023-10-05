58c6e636123788c6f6353ab910536e76b4e1a226
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-05 14:13:26 -0300

    s

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,10 @@ const Button = () => {
             <div></div> {/* Linea horizontal top */}
             <div > {/* Contenedor boton y lineas verticales */}
                 <div></div> {/* Linea vertical izquierda */}
-                <button class="before:ease relative h-12 w-40 overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
+                <button class="before:ease relative h-12 w-40 overflow-hidden border border-black shadow-2xl 
+                before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full 
+                before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white
+                hover:shadow-black hover:before:-rotate-180">
                     <span class="relative z-10">Slide hover</span>
                 </button>
                 <button className='button type3'>prueba</button>
