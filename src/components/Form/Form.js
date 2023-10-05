37bc134ea861c9dd2a4e81d48559736926dcee92
Nazaa29 <lopeznazarenoo@gmail.com>
2023-10-04 22:16:05 -0300

    input arreglado parte mil

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,8 +4,8 @@ import { useState, useRef } from "react";
 
 const Form = () => {
     const inputStyle = 'w-full p-[10px] outline-none border border-gray-custom bg-dark-custom text-white text-[1em] ';
-    const labelStyle = 'absolute left-1 py-[10px] px-[10px] pointer-events-none duration-300 uppercase';
-    const textareaStyle = 'w-full px-3 h-64 py-2  outline-none border border-gray-custom bg-dark-custom text-white text-[1em] resize-none';
+    const labelStyle = 'absolute left-1 px-[10px] pointer-events-none duration-300 uppercase';
+    const textareaStyle = 'w-full px-3 h-64 py-2 outline-none border border-gray-custom bg-dark-custom text-white text-[1em] resize-none';
 
     const [isFocused, setIsFocused] = useState(false);
     const inputRef=useRef(null);
@@ -17,14 +17,16 @@ const Form = () => {
     const blurHandler = () =>{
         if(inputRef.current.value.trim() === ''){
             setIsFocused(false);
+            
         }else{
             setIsFocused(true);
         }
         
     }
-    console.log(inputRef)
 
-    const labelDynamic=' text-red-custom translate-x-[10px] translate-y-[-9px] text-[0.75em] py-[0px] px-[10px] bg-dark-custom border-l-[1px] border-red-custom border-r-[1px] tracking-[0.25em]';
+    const pyClass = isFocused ? 'py-[0px]' : 'py-[10px]';
+
+    const labelDynamic=' text-red-custom translate-x-[10px] translate-y-[-9px] text-[0.75em] px-[10px] bg-dark-custom border-l-[1px] border-red-custom border-r-[1px] tracking-[0.25em]';
 
     return (    
         <div className="max-w-md mx-auto w-full">
@@ -32,7 +34,7 @@ const Form = () => {
                 <Input label="Nombre" type="text" inputStyle={inputStyle} labelStyle={labelStyle} />
                 <Input label="Email" type="email" inputStyle={inputStyle} labelStyle={labelStyle} />
                 <div className="mb-4 relative pt-4">
-                    <label className={`${labelStyle} ${isFocused ? labelDynamic : 'text-gray-custom'}`}>Your Idea</label>
+                    <label className={`${labelStyle} ${isFocused ? labelDynamic : 'text-gray-custom'} ${pyClass}`}>Your Idea</label>
                     <textarea
                         className={`${textareaStyle} ${isFocused ? 'border-red-custom' : 'border-gray-custom'}`}
                         rows="4"
--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -16,13 +16,14 @@ const Input = ({ label, type, inputStyle, labelStyle}) => {
         }
         
     }
-    console.log(inputRef)
 
-    const labelDynamic=' text-red-custom translate-x-[10px] translate-y-[-9px] text-[0.75em] py-[0px] px-[10px] bg-dark-custom border-l-[1px] border-red-custom border-r-[1px] tracking-[0.25em]';
+    const pyClass = isFocused ? 'py-[0px]' : 'py-[10px]';
+
+    const labelDynamic=' text-red-custom translate-x-[10px] translate-y-[-9px] text-[0.75em] px-[10px] bg-dark-custom border-l-[1px] border-red-custom border-r-[1px] tracking-[0.25em]';
 
     return (
         <div className="mb-4 relative pt-4">
-            <label className={`${labelStyle} ${isFocused ? `${labelDynamic}` : 'text-white'}`}>{label}</label>
+            <label className={`${labelStyle} ${isFocused ? `${labelDynamic}` : 'text-white' } ${pyClass}`}>{label}</label>
             <input
                 className={`${inputStyle} ${isFocused ? 'border-red-custom' : 'border-gray-custom'}`}
                 type={type}
