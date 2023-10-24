3efdaafe9fa6092ba8af57251f04607a5541c501
nazitaOG <lopeznazarenoo@gmail.com>
2023-10-23 23:11:31 -0300

    s

---
+++ src/components/Proyects/Testimonial.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Testimonial = ({ name, text, image, bussiness }) => {
+
+    return (
+        <li className="bg-dark-custom text-white h-[45%] p-4 w-[50%] relative shadow-[4px_4px_10px_0px_rgba(0,0,0,0.4)]">
+            <div className=" w-[45%] flex h-[22%] bg-dark-custom -left-10 -top-6 absolute  shadow-[4px_4px_10px_0px_rgba(0,0,0,0.4)]">
+                <img src={image} alt={name} className="w-[23%] h-full object-cover border-r-[2px] border-red-custom" />
+                <div className=' justify-center flex-col flex pl-4'>
+                    <h3 className=" font-bold ">{name}</h3>
+                    <h5 className=" font-thin text-sm">{bussiness}</h5>
+                </div>
+                
+            </div>
+            <div className="mt-12 mb-4 pl-10 pr-10">
+                {text}
+            </div>
+        </li>
+    );
+};
+
+export default Testimonial;
--- src/components/Proyects/TestimonialsSlide.js
+++ src/components/Proyects/TestimonialsSlide.js
@@ -1,12 +1,36 @@
-const TestimonialsSlide = ({setCurrent}) => {
+import Testimonial from "./Testimonial";
+import perfil from '../../assets/images/foto-perfil-1.png'
+import perfil2 from '../../assets/images/perfil2.jpg'
+import { useState } from "react";
+
+const TestimonialsSlide = ({ setCurrent }) => {
     const buttonText = 'PROYECTOS';
     const buttonLetters = buttonText.split('');
+    const [currentTestimonial, setCurrentTestimonial] = useState(0);
+
     const prev = () => {
         setCurrent((current) => current === 0 ? current : current - 1)
     }
 
+    const testimonials = [
+        {
+            name: "Fernando Lopez",
+            bussiness:'Solymar Agencia de Turismo',
+            text: "Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonioTexto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio",
+            image: perfil
+        },
+        {
+            name: "Juan Cruz Dovzak",
+            bussiness:'Marketfy eCommerce',
+            text: "Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonioTexto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio Texto del testimonio 1. Puede ser un párrafo largo que se ajustará al contenedor.Texto del testimonio",
+            image: perfil2
+        },
+        // Puedes agregar más objetos testimonio según sea necesario
+    ];
+
+
     return (
-        <section className="w-full relative">
+        <section className="w-full h-full relative flex flex-col">
             <div className="absolute top-8 bottom-0 right-14 bg-gray-custom w-[1px]"></div>
             {/* Línea horizontal */}
             <div className="absolute top-16 left-24 right-24  bg-gray-custom h-[1px]"></div>
@@ -18,6 +42,23 @@ const TestimonialsSlide = ({setCurrent}) => {
                     </div>
                 ))}
             </div>
+            <ul className="flex justify-center items-center mt-10 h-[100%] w-[100%]">
+                {testimonials.map((testimonial, index) => (
+                    <Testimonial
+                        key={index}
+                        name={testimonial.name}
+                        bussiness={testimonial.bussiness}
+                        text={testimonial.text}
+                        image={testimonial.image}
+                    />
+                ))}
+            </ul>
+            <ul className="flex absolute bottom-24 left-[50%] translate-x-[-50%]">
+                {testimonials.map((_, index) => (
+                    <li className=" rounded-full w-4 h-4 bg-gray-custom m-4"></li>
+                ))}
+            </ul>
+
         </section>
     );
 }
