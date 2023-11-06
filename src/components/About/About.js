dc336a49caed02d530ff83ac7d0f6fbab6775da3
nazitaOG <lopeznazarenoo@gmail.com>
2023-11-06 14:55:44 -0300

    responsive about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,22 +2,22 @@ import Reveal from "../UI/Reveal";
 
 const About = () => {
   return (
-    <section id="about" className="relative flex p-8 bg-gray-custom h-screen">
+    <section id="about" className="relative flex align-middle justify-center p-8 bg-gray-custom h-screen min-h-[650px] ">
       {/* Línea vertical */}
       <div className="absolute top-8 bottom-0 left-14 bg-black w-1px"></div>
       {/* Línea horizontal */}
-      <div className="absolute top-16 left-24 right-24  bg-black h-1px"></div>
+      <div className="absolute md:top-16 top-14 left-24 right-24 bg-black h-1px"></div>
 
       {/* Contenido */}
-      <div className="absolute left-40 top-24 w-7/12">
-        <div className="" data-taos-offset="300"></div>
+      <div className="md:absolute md:left-40 md:top-24 ml-2 md:w-7/12 w-8-12">
+        <div data-taos-offset="300"></div>
         <Reveal>
-          <h1 className="text-6xl font-bold uppercase mt-10 ml-7 font-tungsten tracking-wider animate-fade">
+          <h1 className="md:text-6xl text-5xl text-center md:text-start font-bold uppercase mt-10 font-tungsten tracking-wider animate-fade">
             About Us
           </h1>
         </Reveal>
 
-        <p className="mt-4 ml-7 font-sourceCodePro">
+        <p className="mt-4 ml-10 md:ml-0 md:text-base text-sm font-sourceCodePro">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
           ipsum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,14 +8,14 @@ const Home = (props) => {
     <section
       id="home"
       className="w-full flex bg-dark-custom justify-center items-center relative"
-      style={{ height: dynamicHeight, minHeight: dynamicHeight }}
+      style={{ height: dynamicHeight, minHeight: 270}}
     >
       <div className="w-full h-full relative flex-shrink-0">
         <ParticleScene />
       </div>
       <div className="z-10 absolute inset-0 flex flex-col justify-center items-center">
         <div className="flex flex-col items-start ml-6">
-          <h1 className="sm:text-5xl md:text-7xl text-3xl">MUGENCODE ™</h1>
+          <h1 className="sm:text-6xl md:text-7xl text-4xl">MUGENCODE ™</h1>
           <div className="flex flex-col items-start">
             <div className="ml-6 w-20 h-1 mt-2 rounded bg-red-custom my-2"></div>
             <div className="ml-12 w-20 h-1 mt-1 rounded bg-red-custom"></div>
