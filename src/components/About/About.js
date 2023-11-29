a3e1065f9f6513322a3007c48d7227a2a1e9ede5
OriginalKazdov <juandovzak@gmail.com>
2023-11-29 01:27:55 -0300

    Corta la bocha BarraHabilidad
    crash course del lopeee

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
     { nombre: "CSS", nivel: 90, icono: iconoCSS },
     { nombre: "Tailwind", nivel: 60, icono: iconoTW },
     { nombre: "HTML", nivel: 100, icono: iconoHTML },
-    { nombre: "Gaming", nivel: 9999, icono: iconoG },
+    { nombre: "Gaming", nivel: 100, icono: iconoG },
   ];
 
   return (
@@ -35,47 +35,51 @@ const About = () => {
             About Us
           </h1>
         </Reveal>
-        <div className="lg:w-3/4 flex flex-col lg:flex-row mt-8 mb-20 ml-20 sm:ml-24 lg:ml-[181px] h-fit mr-16 lg:top-36 lg:left-[96px]">
-          <p className="sm:text-sm text-xs xl:text-base font-sourceCodePro">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsum ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsum ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsumipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsum
-          </p>
-        </div>
-        <div className="contenedor-habilidades flex flex-wrap justify-center">
-          <ul className="lista-habilidades w-full">
-            {habilidades.map((habilidad, index) => (
-              <li key={index} className="elemento-lista-habilidades">
-                <BarraHabilidad habilidad={habilidad} />
-              </li>
-            ))}
-          </ul>
+
+        <div className="xl:flex">
+          <div className="xl:w-[48%] lg:w-[64%] flex flex-col lg:flex-row mt-8 mb-20 ml-20 sm:ml-24 lg:ml-[181px] h-fit xl:mr-10 mr-16 lg:top-36 lg:left-[96px] 2xl:mt-10">
+            <p className="sm:text-sm text-xs xl:text-base font-sourceCodePro 2xl:text-[18px]">
+              Hola, soy [Tu Nombre], un apasionado y dedicado desarrollador Full
+              Stack con experiencia en crear soluciones web eficientes y
+              atractivas. Me especializo en el stack MERN, que incluye MongoDB,
+              Express.js, React.js y Node.js, combinando estas poderosas
+              tecnologías para construir aplicaciones web desde el backend hasta
+              el frontend. Mi viaje en el mundo del desarrollo web comenzó hace
+              [X años], impulsado por mi curiosidad y amor por la tecnología.
+              Desde entonces, he trabajado en varios proyectos, desde sitios web
+              empresariales hasta aplicaciones web interactivas, siempre con el
+              objetivo de entregar productos que no solo cumplan con los
+              requisitos técnicos sino que también proporcionen una experiencia
+              de usuario excepcional. En el frontend, utilizo principalmente
+              React.js, aprovechando su eficiente renderizado y la capacidad de
+              construir interfaces de usuario dinámicas y reactivas. Para los
+              estilos, soy adepto en CSS y frameworks como Tailwind CSS, que me
+              permite crear diseños responsivos y visualmente atractivos con
+              eficiencia y precisión. En el backend, mi experiencia con Node.js
+              y Express.js me permite construir servidores robustos y
+              escalables. La gestión de bases de datos con MongoDB complementa
+              mis habilidades en el backend, permitiéndome manejar datos
+              complejos y garantizar que las aplicaciones funcionen sin
+              problemas. Constantemente busco nuevas tecnologías y metodologías
+              para mejorar mis habilidades y mantenerme al día con las
+              tendencias actuales en desarrollo web. La innovación y la
+              adaptabilidad son clave en este campo, y estoy comprometido a
+              aprender y crecer continuamente como profesional. Mi objetivo es
+              colaborar en proyectos desafiantes y significativos donde pueda
+              aplicar mi experiencia y al mismo tiempo aprender y crecer. Estoy
+              ansioso por conectar con personas y empresas que compartan mi
+              pasión por crear soluciones digitales excepcionales.
+            </p>
+          </div>
+          <div className="xl:w-[20%] 2xl:w-[25%]">
+            <ul className="lista-habilidades">
+              {habilidades.map((habilidad, index) => (
+                <li key={index} className="elemento-lista-habilidades">
+                  <BarraHabilidad habilidad={habilidad} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </section>
--- src/components/About/BarraHabilidad.js
+++ src/components/About/BarraHabilidad.js
@@ -1,29 +1,59 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "../../assets/styles/barrahabilidad.css";
 
 const BarraHabilidad = ({ habilidad }) => {
   const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      setIsVisible(true);
+    }
+  };
 
   useEffect(() => {
-    if (ref.current) {
+    const observer = new IntersectionObserver(callbackFunction, {
+      root: null,
+      threshold: 0.1, // Ajusta según tus necesidades
+    });
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, [ref]);
+
+  useEffect(() => {
+    if (isVisible && ref.current) {
       ref.current.style.width = `${habilidad.nivel}%`;
     }
-  }, [habilidad.nivel]);
+  }, [isVisible, habilidad.nivel]);
 
   return (
-    <div className="contenedor-habilidad">
-      <div className="icono-habilidad">
-        <img src={habilidad.icono} alt={`${habilidad.nombre} icon`} />
+    <div className="xl:mt-7 ml-16 xl:ml-0 md:ml-24 sm:ml-20 lg:ml-[181px] flex items-center justify-start mb-4 contenedor-habilidad">
+      <div className="icono-habilidad sm:mr-2.5 mr-1 flex mt-3">
+        <img
+          className="sm:w-11 sm:h-11 w-9 h-9 object-contain"
+          src={habilidad.icono}
+          alt={`${habilidad.nombre} icon`}
+        />
       </div>
-      <div className="info-habilidad">
-        <div className="titulo-y-porcentaje">
-          <span className="nombre-habilidad">{habilidad.nombre}</span>
-          <span className="porcentaje-habilidad">{habilidad.nivel}%</span>
+      <div className="info-habilidad flex-1 flex flex-col">
+        <div className="titulo-y-porcentaje flex justify-between items-center xl:w-[100%] lg:w-[70%] w-[86%]">
+          <span className="nombre-habilidad m-[5px] font-semibold md:font-bold md:text-lg text-sm">
+            {habilidad.nombre}
+          </span>
+          <span className="porcentaje-habilidad m-[5px] font-semibold md:font-bold md:text-lg text-sm">
+            {habilidad.nivel}%
+          </span>
         </div>
-        <div className="barra-habilidad">
+        <div className="barra-habilidad bg-[#ddd] rounded-[15px] overflow-hidden xl:w-[100%] lg:w-[70%] w-[86%]">
           <div
             ref={ref}
-            className="progreso-habilidad"
-            style={{ width: 0, backgroundColor: habilidad.color }} // Inicia en 0
+            className="progreso-habilidad h-[10px] rounded-[15px]"
+            style={{ width: 0, backgroundColor: habilidad.color }}
           ></div>
         </div>
       </div>
