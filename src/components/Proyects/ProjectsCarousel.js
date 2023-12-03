087390725c26733b8fe928f773430b11854401f4
OriginalKazdov <juandovzak@gmail.com>
2023-12-03 02:32:49 -0300

    Cuasi terminado toda la pagina
    adasdasdasd

--- src/components/Proyects/ProjectsCarousel.js
+++ src/components/Proyects/ProjectsCarousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -6,39 +6,65 @@ import IconoReact from "../../assets/images/reactjs.png";
 import IconoJS from "../../assets/images/javascript.png";
 import { FaGlobe } from "react-icons/fa";
 
-// Componente individual para cada proyecto
-const Proyecto = ({ datos }) => (
-  <div className="relative bg-dark-custom shadow-[4px_4px_10px_0px_rgba(0,0,0,0.4)] mb-3 mx-auto w-[200px] h-[400px] sm:w-[300px] sm:h-[500px]">
-    <img
-      src={datos.imagenUrl}
-      alt={datos.nombre}
-      className="w-full object-cover border-b-2 border-red-custom h-[150px]"
-    />
-    <div className="p-4">
-      <div className="flex items-center mt-2 justify-between">
-        <p className="text-gray-custom text-xl mr-2 whitespace-nowrap font-semibold tracking-wider">
-          {datos.nombre}
-        </p>
+const useWindowSize = () => {
+  const [size, setSize] = useState([window.innerWidth]);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize([window.innerWidth]);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return size;
+};
 
-        <div className="w-[60%] h-0 mt-2 border-t-[1px] border-t-red-custom"></div>
-        <a
-          href={datos.enlaceSitio}
-          className="text-gray-custom text-2xl mt-1 ml-2 hover:text-gray-400 duration-300"
-        >
-          <FaGlobe />
-        </a>
+// Componente individual para cada proyecto
+const Proyecto = ({ datos }) => {
+  const [width] = useWindowSize();
+  const isSmallScreen = width < 640; // sm breakpoint
+
+  return (
+    <div className="relative bg-dark-custom shadow-[4px_4px_10px_0px_rgba(0,0,0,0.4)] mb-3 mx-auto w-[200px] h-[400px] sm:w-[300px] sm:h-[500px] xl:w-[382px] xl:h-[582px]">
+      <img
+        src={datos.imagenUrl}
+        alt={datos.nombre}
+        className="w-full object-cover border-b-2 border-red-custom h-[150px] xl:h-[200px]"
+      />
+      <div className="p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-gray-custom text-lg sm:text-xl lg:text-base xl:text-xl mr-2 whitespace-nowrap font-semibold tracking-wider mb-1 sm:mb-2">
+            {datos.nombre}
+          </p>
+
+          <div className="w-[60%] h-0 mt-1 sm:mt-2 border-t-[1px] border-t-red-custom"></div>
+          <a
+            href={datos.enlaceSitio}
+            className="text-gray-custom text-xl mt-1 ml-2 hover:text-gray-400 duration-300"
+          >
+            <FaGlobe />
+          </a>
+        </div>
+        <p className="text-red-custom text-sm sm:text-base lg:text-base xl:text-xl mb-1 sm:mb-2">
+          {datos.tecnologias}
+        </p>
+        <div className="mt-1 sm:mt-2">
+          <span className="text-sm sm:text-base lg:text-base xl:text-xl text-gray-custom mb-1 sm:mb-2">
+            {isSmallScreen ? datos.descripcionCorta : datos.descripcion}
+          </span>
+          <a
+            href="www.vacacay.com"
+            className="ml-2 text-red-custom text-sm sm:text-base hover:underline"
+          >
+            Visitenos &gt;
+          </a>
+        </div>
       </div>
-      <p className="text-red-custom mt-4">{datos.tecnologias}</p>
-      <div className="mt-4 ">
-        <span className="text-base text-gray-custom">{datos.descripcion}</span>
-        <a href="www.vacacay.com" className="ml-2 text-red-custom text-base hover:underline">Visitenos &gt;</a>
-      </div>
+      <p className="absolute bottom-2 right-2 text-[8px] sm:text-[10px] xl:text-[12px] text-gray-custom">
+        Powered by MUGENCODE
+      </p>
     </div>
-    <p className="absolute bottom-2 right-2 text-[10px] text-gray-custom">
-      Powered by MUGENCODE
-    </p>
-  </div>
-);
+  );
+};
 
 const ProjectsCarousel = () => {
   const proyectos = [
@@ -46,7 +72,9 @@ const ProjectsCarousel = () => {
       nombre: "Proyecto 1",
       imagenUrl: IconoJS,
       descripcion:
-        "Este proyecto lo hicimos en 2023 mogolico mogolico mogolico Este proyecto lo hicimos en 2023 mogolico",
+        "Este proyecto lo hicimos en 2023 mogolico mogolico mogolico Este proyecto lo hicimos en 2023 mogolico Este proyecto lo hicimos en 2023 mogolico mogolico mogolico",
+      descripcionCorta:
+        "Descripcion corta para todas las wachas cosa de que no joda me entendes?",
       tecnologias: "JavaScript - Node.js - Express", // Añadido
       enlaces: [
         { url: "#", text: "GitHub" },
@@ -58,6 +86,7 @@ const ProjectsCarousel = () => {
       nombre: "Proyecto 2",
       imagenUrl: IconoReact,
       descripcion: "Descripción extendida del Proyecto 2.",
+      descripcionCorta: "",
       tecnologias: "React - Redux - Tailwind", // Añadido
       enlaces: [
         { url: "#", text: "GitHub" },
@@ -69,6 +98,7 @@ const ProjectsCarousel = () => {
       nombre: "Proyecto 3",
       imagenUrl: IconoJS,
       descripcion: "Descripción extendida del Proyecto 1.",
+      descripcionCorta: "",
       tecnologias: "JavaScript - Node.js - Express", // Añadido
       enlaces: [
         { url: "#", text: "GitHub" },
@@ -80,6 +110,7 @@ const ProjectsCarousel = () => {
       nombre: "Proyecto 4",
       imagenUrl: IconoReact,
       descripcion: "Descripción extendida del Proyecto 2.",
+      descripcionCorta: "",
       tecnologias: "React - Redux - Tailwind", // Añadido
       enlaces: [
         { url: "#", text: "GitHub" },
