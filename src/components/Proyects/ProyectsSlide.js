8562821fa0ec816df8bece0649673c6cdf267292
OriginalKazdov <juandovzak@gmail.com>
2023-12-06 22:09:06 -0300

    desaparece la linea blanca descenso en la locura

--- src/components/Proyects/ProyectsSlide.js
+++ src/components/Proyects/ProyectsSlide.js
@@ -1,7 +1,18 @@
 import Reveal from "../UI/Reveal";
 import ProyectoSwiper from "./ProyectoSwiper";
+import React, { useState, useEffect } from "react";
 
 const ProyectsSlide = ({ setCurrent }) => {
+  const [showVerticalLine, setShowVerticalLine] = useState(
+    window.innerWidth > 640
+  );
+
+  useEffect(() => {
+    const handleResize = () => setShowVerticalLine(window.innerWidth > 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const buttonText = "TESTIMONIOS";
   const buttonLetters = buttonText.split("");
   const next = () => {
@@ -11,14 +22,17 @@ const ProyectsSlide = ({ setCurrent }) => {
 
   return (
     <section className="w-full relative h-full mb-10 sm:pb-12">
-      <div className="absolute top-8 bottom-0 left-8 md:left-14 bg-gray-custom w-[1px]"></div>
+      <div
+        className="absolute top-8 bottom-0 left-8 md:left-14 bg-gray-custom w-[1px]"
+        style={{ display: showVerticalLine ? "block" : "none" }}
+      ></div>
       <div className="absolute w-4/5 top-16 md:left-24 left-20 right-24 h-1px invisible sm:visible bg-gray-custom"></div>
       <Reveal>
         <h1 className="lg:text-6xl  sm:text-5xl text-4xl  mt-12 lg:mt-24 lg:ml-[182px] sm:mt-[100px] font-bold uppercase font-tungsten tracking-wider text-gray-custom text-center lg:text-start w-screen">
           Proyectos
         </h1>
       </Reveal>
-      <ProyectoSwiper/>
+      <ProyectoSwiper />
       <div
         className="absolute right-0 top-0 p-4 flex flex-col font-workSans items-center bg-black bg-opacity-25 justify-center text-gray-custom hover:text-red-custom hover:bg-opacity-50 h-full transition-all ease-in-out duration-500 cursor-pointer invisible lg:visible"
         onClick={next}
