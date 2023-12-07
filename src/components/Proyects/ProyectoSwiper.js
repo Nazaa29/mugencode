973203c112a02f52bae3df720b9ecd2c7d217cc3
nazitaOG <lopeznazarenoo@gmail.com>
2023-12-06 21:15:33 -0300

    ahora si

--- src/components/Proyects/ProyectoSwiper.js
+++ src/components/Proyects/ProyectoSwiper.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import IconoReact from "../../assets/images/reactjs.png";
 import IconoJS from "../../assets/images/javascript.png";
 import { FaGlobe } from "react-icons/fa";
@@ -72,17 +72,23 @@ export default function ProyectoSwiper() {
     },
   ];
 
-  const [slidesPerView, setSlidesPerView] = useState(2);
+    const [slidesPerView, setSlidesPerView] = useState(2);
+    const [arrow, setArrow] = useState(true);
 
-  useEffect(() => {
-    const handleResize = () => {
-      // Ajustar el número de slides por vista en función del tamaño de la pantalla
-      if (window.innerWidth < 640) {
-        setSlidesPerView(1);
-      } else {
-        setSlidesPerView(2);
-      }
-    };
+    useEffect(() => {
+        const handleResize = () => {
+            // Ajustar el número de slides por vista en función del tamaño de la pantalla
+            if (window.innerWidth < 640) {
+                setSlidesPerView(1);
+                setArrow(false);
+            } else if (window.innerWidth < 1024) {
+                setSlidesPerView(1);
+                setArrow(true);
+            } else {
+                setSlidesPerView(2);
+                setArrow(true);
+            }
+        };
 
     window.addEventListener("resize", handleResize);
 
@@ -93,64 +99,61 @@ export default function ProyectoSwiper() {
     };
   }, []);
 
-  return (
-    <Swiper
-      modules={[Navigation, Pagination]}
-      spaceBetween={0}
-      slidesPerView={slidesPerView}
-      navigation
-      pagination={{ clickable: true }}
-      style={{ width: "70%", height: " 77%", margin: "auto" }}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {proyectos.map((proyecto, index) => (
-        <SwiperSlide>
-          <div
-            key={index}
-            className="mt-8 relative bg-dark-custom shadow-[4px_4px_10px_0px_rgba(0,0,0,0.4)] w-[58%] h-[87%] mb-3 mx-auto "
-          >
-            <img
-              src={proyecto.imagenUrl}
-              alt={proyecto.nombre}
-              className="w-full object-cover border-b-2 border-red-custom h-[150px] xl:h-[200px]"
-            />
-            <div className="p-4">
-              <div className="flex items-center mb-3 justify-between">
-                <p className="text-gray-custom text-xl mr-2 whitespace-nowrap font-semibold tracking-wider">
-                  {proyecto.nombre}
-                </p>
-                <div className="w-[60%] h-0 mt-1 sm:mt-2 border-t-[1px] border-t-red-custom"></div>
-                <a
-                  href={proyecto.enlaceSitio}
-                  className="text-gray-custom text-xl mt-1 ml-2 hover:text-gray-400 duration-300"
-                >
-                  <FaGlobe />
-                </a>
-              </div>
-              <p className="text-red-custom text-base font-semibold mb-3">
-                {proyecto.tecnologias}
-              </p>
-              <div className="mt-1 sm:mt-2">
-                <span className="text-base text-gray-custom pb-2">
-                  {isLargeScreen
-                    ? proyecto.descripcion
-                    : proyecto.descripcionCorta}
-                </span>
-                <a
-                  href={proyecto.enlace}
-                  className="ml-2 text-red-custom text-base hover:underline"
-                >
-                  Visitenos &gt;
-                </a>
-              </div>
-            </div>
-            <p className="absolute bottom-2 right-2 text-[8px] sm:text-[10px] xl:text-[12px] text-gray-custom">
-              Powered by MUGENCODE
-            </p>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
-}
+    return (
+        <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={0}
+            slidesPerView={
+                slidesPerView
+            }
+            navigation={arrow}
+            pagination={{ clickable: true }}
+            style={{ width: '70%', height: " 77%", margin: 'auto', }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            {proyectos.map((proyecto, index) => (
+                <SwiperSlide>
+                    <div key={index} className="mt-8 relative max-w-[300px] min-w-[150px] bg-dark-custom shadow-[4px_4px_10px_0px_rgba(0,0,0,0.4)] w-[58%] lg:w-[68%] h-[87%] mb-3 mx-auto ">
+                        <img
+                            src={proyecto.imagenUrl}
+                            alt={proyecto.nombre}
+                            className="w-full object-cover border-b-2 border-red-custom h-[150px] xl:h-[200px]"
+                        />
+                        <div className="p-4">
+                            <div className="flex items-center mb-3 justify-between">
+                                <p className="text-gray-custom text-lg sm:text-xl mr-2 whitespace-nowrap font-semibold tracking-wider">
+                                    {proyecto.nombre}
+                                </p>
+                                <div className="w-[60%] hidden lg:visible lg:flex h-0 mt-1 border-t-[1px] border-t-red-custom"></div>
+                                <a
+                                    href={proyecto.enlaceSitio}
+                                    className="text-gray-custom text-lg sm:text-xl ml-2 hover:text-gray-400 duration-300"
+                                >
+                                    <FaGlobe />
+                                </a>
+                            </div>
+                            <p className="text-red-custom text-sm sm:text-base font-semibold mb-3">
+                                {proyecto.tecnologias}
+                            </p>
+                            <div className="mt-1 sm:mt-2">
+                                <span className="text-sm sm:text-base text-gray-custom pb-2">
+                                    {proyecto.descripcion}
+                                </span>
+                                <a
+                                    href={proyecto.enlace}
+                                    className="ml-2 text-red-custom text-sm sm:text-base hover:underline"
+                                >
+                                    Visitenos &gt;
+                                </a>
+                            </div>
+                        </div>
+                        <p className="absolute bottom-2 right-2 text-[8px] sm:text-[10px] xl:text-[12px] text-gray-custom">
+                            Powered by MUGENCODE
+                        </p>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+};
